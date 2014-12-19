78d41733e0ad80e2753edbd30dddf75e13482013
Felix Moessbauer <moessbauer@cip.ifi.lmu.de>
2014-12-19 20:55:33 +0100

    main methods created. Project structure added

--- NineMorris/main.hs
+++ NineMorris/main.hs
@@ -2,6 +2,20 @@
 Main file of the Haskell-NM-9Morris Project
 -}
 import NineMorris.Client
+import NineMorris.Globals as Globals
+import Paths_haskell_nm_9morris (version)
+import Data.Version (showVersion)
+import System.Environment -- <= für Command Line Arguments
+
+printHelp :: IO()
+printHelp = putStrLn "Usage: <gameid> (<path to config File>)"
 
 main :: IO()
-main = undefined
+main = do
+    putStrLn $ "Starting Client Version " ++ showVersion version
+    params <- getArgs
+    case params of
+        (gameid:path:xs) -> return $ startClient gameid path
+        (gameid:xs) -> return $ startClient gameid Globals.defaultConfig
+        _ -> do return $ printHelp
+    putStrLn "Stop Client"
