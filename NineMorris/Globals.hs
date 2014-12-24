5ac3a8c96fe6d94d2787c74bd4639d2f52345f64
Felix Moessbauer <moessbauer@cip.ifi.lmu.de>
2014-12-24 01:29:14 +0100

    Protocol implementation till gamekind verification step

--- NineMorris/Globals.hs
+++ NineMorris/Globals.hs
@@ -6,12 +6,13 @@ import Data.Typeable
 
 defaultConfig   = "config.ini"  :: String
 gameIdLength    = 11            :: Int
+internalVersion = "1.0"         :: String
 
 data Config = Config {hostname::String, port::Int, gamekind::String} deriving (Show)
 type Gameid = String
 
 {- Game Excemptions -}
-data MorrisException = GameIdNotValid | FileNotFound | ConfigNotValid String
+data MorrisException = GameIdNotValid | FileNotFound | ConfigNotValid String | ProtocolError String
     deriving (Show, Typeable)
 
 instance Exception MorrisException
