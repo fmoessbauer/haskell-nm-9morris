37ced1233a40190e1c9aef6919160f01099475aa
Felix Moessbauer <moessbauer@cip.ifi.lmu.de>
2015-01-07 10:50:01 +0100

    AI Interface seperated and moved to own file

---
+++ NineMorris/AI/Interface.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+module NineMorris.AI.Interface (
+    convertMove,
+    convertBoard )
+where
+
+import qualified NineMorris.Globals as G
+import qualified NineMorris.AI as AI
+import Data.Text (Text,append)
+import Control.Exception
+import qualified Data.Map as Map
+
+convertMove :: Maybe AI.Move -> Text
+convertMove Nothing = throw G.AiException
+convertMove (Just AI.FullMove { AI.fstAction=fsta, AI.sndAction=snda}) = 
+    let
+        partOne = convertFirstAction fsta
+        partTwo = case snda of
+                    Nothing     -> ""
+                    (Just act)  -> ";" `append` convertSecondAction act
+    in partOne `append` partTwo
+
+convertFirstAction :: AI.FirstAction -> Text
+convertFirstAction (AI.Place pos)     = (convertToServerPos $ pos)
+convertFirstAction (AI.Move old new)  = (convertToServerPos $ old) `append` ":" `append` (convertToServerPos $ new)
+
+convertSecondAction :: AI.SecondAction -> Text
+convertSecondAction (AI.Take pos) = convertToServerPos pos
+
+convertToInternalPos :: Text -> AI.Position
+convertToInternalPos pos =  AI.Position (Map.findWithDefault (-1) pos G.toAiPositions)
+
+convertToServerPos :: AI.Position -> Text
+convertToServerPos (AI.Position pos) = Map.findWithDefault "" pos G.toServerPositions
+
+
+convertBoard :: G.PlayerInfo -> [G.StoneInfo] -> AI.Board
+convertBoard player stones = foldl (convertSingleStone $ player) (AI.newBoard) stones
+
+{- I am Black -}
+convertSingleStone :: G.PlayerInfo -> AI.Board -> G.StoneInfo -> AI.Board
+convertSingleStone (G.PlayerInfo {G.pid=pid}) (board) (G.StoneInfo {G.spid=playerId, G.sposition=pos})
+    | pos == "A"       = AI.setBoardPosition Nothing (convertToInternalPos pos) board
+    | playerId == pid  = (AI.reduceBoardHandCount AI.Black) $ (AI.setBoardPosition (Just AI.Black) (convertToInternalPos pos) board)
+    | otherwise        = (AI.reduceBoardHandCount AI.Red) $ (AI.setBoardPosition (Just AI.Red)   (convertToInternalPos pos) board)
--- NineMorris/Connector.hs
+++ NineMorris/Connector.hs
@@ -11,6 +11,7 @@ import Data.Text (Text,pack,unpack,append)
 import qualified Data.Text.IO as TextIO
 import NineMorris.Parsers.Protocol
 import qualified NineMorris.AI as AI
+import NineMorris.AI.Interface
 
 import qualified Data.Map as Map
 
@@ -160,40 +161,6 @@ parseEndplayers :: Text -> IO ()
 parseEndplayers str = if str == "+ ENDPLAYERS" -- todo switch to parseStatic
     then return ()
     else throw $ G.ProtocolError ("ENDPLAYERS expected, but: " `append` str)
-
-convertMove :: Maybe AI.Move -> Text
-convertMove Nothing = throw G.AiException
-convertMove (Just AI.FullMove { AI.fstAction=fsta, AI.sndAction=snda}) = 
-    let
-        partOne = convertFirstAction fsta
-        partTwo = case snda of
-                    Nothing     -> ""
-                    (Just act)  -> ";" `append` convertSecondAction act
-    in partOne `append` partTwo
-
-convertFirstAction :: AI.FirstAction -> Text
-convertFirstAction (AI.Place pos)     = (convertToServerPos $ pos)
-convertFirstAction (AI.Move old new)  = (convertToServerPos $ old) `append` ":" `append` (convertToServerPos $ new)
-
-convertSecondAction :: AI.SecondAction -> Text
-convertSecondAction (AI.Take pos) = convertToServerPos pos
-
-convertToInternalPos :: Text -> AI.Position
-convertToInternalPos pos =  AI.Position (Map.findWithDefault (-1) pos G.toAiPositions)
-
-convertToServerPos :: AI.Position -> Text
-convertToServerPos (AI.Position pos) = Map.findWithDefault "" pos G.toServerPositions
-
-
-convertBoard :: G.PlayerInfo -> [G.StoneInfo] -> AI.Board
-convertBoard player stones = foldl (convertSingleStone $ player) (AI.newBoard) stones
-
-{- I am Black -}
-convertSingleStone :: G.PlayerInfo -> AI.Board -> G.StoneInfo -> AI.Board
-convertSingleStone (G.PlayerInfo {G.pid=pid}) (board) (G.StoneInfo {G.spid=playerId, G.sposition=pos})
-    | pos == "A"       = AI.setBoardPosition Nothing (convertToInternalPos pos) board
-    | playerId == pid  = (AI.reduceBoardHandCount AI.Black) $ (AI.setBoardPosition (Just AI.Black) (convertToInternalPos pos) board)
-    | otherwise        = (AI.reduceBoardHandCount AI.Red) $ (AI.setBoardPosition (Just AI.Red)   (convertToInternalPos pos) board)
 
 getDebugLine :: Handle -> IO Text
 getDebugLine hdl = do
